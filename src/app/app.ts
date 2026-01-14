345022a74671546edf186e66e6780e924891b781
Teresa Torres <ttorrestorre@uoc.edu>
2026-01-15 00:18:59 +0100

    PEC4 E5  fixed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar';
-
-import { AppRoutingModule } from './app-routing-module';
-
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    NavbarComponent,
-    AppRoutingModule,
-    RouterOutlet,
-  
-  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class AppComponent {
-
-  
-
-}
+export class AppComponent {}
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,3 +1,4 @@
+import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 type View = 'home' | 'articles' | 'new-template' | 'new-reactive';
@@ -10,11 +11,9 @@ type View = 'home' | 'articles' | 'new-template' | 'new-reactive';
   styleUrls: ['./navbar.css']
 })
 export class NavbarComponent {
-
   @Output() sectionChange = new EventEmitter<View>();
 
   select(view: View) {
     this.sectionChange.emit(view);
   }
-
 }
